3592b8f67f898671c928b0dd69ee4fffbc3b01bc
Ofir Sudai <ofirsud@gmail.com>
2022-10-25 17:21:45 -0400

    feat: fixed backffill logic

--- src/jobs/backfill/backfill-bid-activities-collection-id.ts
+++ src/jobs/backfill/backfill-bid-activities-collection-id.ts
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { Queue, QueueScheduler, Worker } from "bullmq";
+import { Job, Queue, QueueScheduler, Worker } from "bullmq";
 import { randomUUID } from "crypto";
+import _ from "lodash";
 
 import { idb } from "@/common/db";
 import { logger } from "@/common/logger";
@@ -24,84 +25,113 @@ new QueueScheduler(QUEUE_NAME, { connection: redis.duplicate() });
 if (config.doBackgroundWork) {
   const worker = new Worker(
     QUEUE_NAME,
-    async () => {
+    async (job: Job) => {
+      let cursor = job.data.cursor as CursorInfo;
+      let continuationFilter = "";
+
       const limit = (await redis.get(`${QUEUE_NAME}-limit`)) || 1;
+
+      if (!cursor) {
+        const cursorJson = await redis.get(`${QUEUE_NAME}-next-cursor`);
+
+        if (cursorJson) {
+          cursor = JSON.parse(cursorJson);
+        }
+      }
+
+      if (cursor) {
+        continuationFilter = `WHERE (a.id) > ($/activityId/)`;
+      }
 
       const results = await idb.manyOrNone(
         `
-UPDATE activities SET
-                collection_id = x."collectionId"
-              FROM (
-SELECT
-a.id as "activityId", o."collectionId"
-FROM activities a
-LEFT JOIN LATERAL (
-                SELECT 
-                    (
-          CASE
-            WHEN orders.token_set_id LIKE 'token:%' THEN
-              (SELECT
-                collections.id              FROM tokens
-              JOIN collections
-                ON tokens.collection_id = collections.id
-              WHERE tokens.contract = decode(substring(split_part(orders.token_set_id, ':', 2) from 3), 'hex')
-                AND tokens.token_id = (split_part(orders.token_set_id, ':', 3)::NUMERIC(78, 0)))
-
-            WHEN orders.token_set_id LIKE 'contract:%' THEN
-              (SELECT
-                collections.id              FROM collections
-              WHERE collections.id = substring(orders.token_set_id from 10))
-
-            WHEN orders.token_set_id LIKE 'range:%' THEN
-              (SELECT
-                collections.id              FROM collections
-              WHERE collections.id = substring(orders.token_set_id from 7))
-
-            WHEN orders.token_set_id LIKE 'list:%' THEN
-              (SELECT
-                CASE
-                  WHEN token_sets.attribute_id IS NULL THEN
-                    (SELECT
-                      collections.id
-                    FROM collections
-                    WHERE token_sets.collection_id = collections.id)
-                  ELSE
-                    (SELECT
-                      collections.id                    FROM attributes
-                    JOIN attribute_keys
-                    ON attributes.attribute_key_id = attribute_keys.id
-                    JOIN collections
-                    ON attribute_keys.collection_id = collections.id
-                    WHERE token_sets.attribute_id = attributes.id)
-                END  
-              FROM token_sets
-              WHERE token_sets.id = orders.token_set_id AND token_sets.schema_hash = orders.token_set_schema_hash)
-            ELSE a.collection_id
-          END
-        ) AS "collectionId"
-                FROM orders
-                WHERE a.order_id = orders.id
-             ) o ON TRUE
-WHERE a.type = 'bid' and a.collection_id is null
-LIMIT $/limit/
-
+                UPDATE activities SET
+                                collection_id = x."collectionId"
+                              FROM (
+                SELECT
+                a.id as "activityId", o."collectionId"
+                FROM activities a
+                LEFT JOIN LATERAL (
+                                SELECT 
+                                    (
+                          CASE
+                            WHEN orders.token_set_id LIKE 'token:%' THEN
+                              (SELECT
+                                collections.id              FROM tokens
+                              JOIN collections
+                                ON tokens.collection_id = collections.id
+                              WHERE tokens.contract = decode(substring(split_part(orders.token_set_id, ':', 2) from 3), 'hex')
+                                AND tokens.token_id = (split_part(orders.token_set_id, ':', 3)::NUMERIC(78, 0)))
+                
+                            WHEN orders.token_set_id LIKE 'contract:%' THEN
+                              (SELECT
+                                collections.id              FROM collections
+                              WHERE collections.id = substring(orders.token_set_id from 10))
+                
+                            WHEN orders.token_set_id LIKE 'range:%' THEN
+                              (SELECT
+                                collections.id              FROM collections
+                              WHERE collections.id = substring(orders.token_set_id from 7))
+                
+                            WHEN orders.token_set_id LIKE 'list:%' THEN
+                              (SELECT
+                                CASE
+                                  WHEN token_sets.attribute_id IS NULL THEN
+                                    (SELECT
+                                      collections.id
+                                    FROM collections
+                                    WHERE token_sets.collection_id = collections.id)
+                                  ELSE
+                                    (SELECT
+                                      collections.id                    FROM attributes
+                                    JOIN attribute_keys
+                                    ON attributes.attribute_key_id = attribute_keys.id
+                                    JOIN collections
+                                    ON attribute_keys.collection_id = collections.id
+                                    WHERE token_sets.attribute_id = attributes.id)
+                                END  
+                              FROM token_sets
+                              WHERE token_sets.id = orders.token_set_id AND token_sets.schema_hash = orders.token_set_schema_hash)
+                            ELSE a.collection_id
+                          END
+                        ) AS "collectionId"
+                                FROM orders
+                                WHERE a.order_id = orders.id
+                             ) o ON TRUE
+                WHERE a.type = 'bid' and a.collection_id is null
+                ${continuationFilter}
+                ORDER BY a.id
+                LIMIT $/limit/
               ) x
               WHERE activities.id = x."activityId"
               RETURNING activities.id
-              
           `,
         {
+          activityId: cursor?.activityId,
           limit,
         }
       );
 
-      logger.info(QUEUE_NAME, `Updated ${results.length} activities.  limit=${limit}`);
+      let nextCursor;
 
-      if (results.length) {
-        await addToQueue();
-      } else {
-        logger.info(QUEUE_NAME, `Done.  limit=${limit}`);
+      if (results.length == limit) {
+        const lastResult = _.last(results);
+
+        nextCursor = {
+          activityId: lastResult.id,
+        };
+
+        await redis.set(`${QUEUE_NAME}-next-cursor`, JSON.stringify(nextCursor));
+
+        await addToQueue(nextCursor);
       }
+
+      logger.info(
+        QUEUE_NAME,
+        `Processed ${results.length} activities.  limit=${limit}, cursor=${JSON.stringify(
+          cursor
+        )}, nextCursor=${JSON.stringify(nextCursor)}`
+      );
     },
     { connection: redis.duplicate(), concurrency: 1 }
   );
