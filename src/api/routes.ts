425e0cc4d48ce1d1c242828f39ed5d8082d5a31f
Jaden Durnford <jadendurnford@gmail.com>
2022-11-17 13:04:39 -0500

    feat: new get tokens bootstrap version with source object

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -944,6 +944,12 @@ export const setupRoutes = (server: Server) => {
 
   server.route({
     method: "GET",
+    path: "/tokens/bootstrap/v2",
+    options: tokensEndpoints.getTokensBootstrapV2Options,
+  });
+
+  server.route({
+    method: "GET",
     path: "/tokens/details/v2",
     options: tokensEndpoints.getTokensDetailsV2Options,
   });
